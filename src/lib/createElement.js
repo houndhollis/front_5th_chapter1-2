62a6cf35066eaf83a2c1dacae821d77773ef04bb
houndhollis <houndhollis9797@gmail.com>
2025-03-31 22:54:18 +0900

    feat: createElement 기능 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,38 @@
+// eslint-disable-next-line no-unused-vars
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    throw new Error("안돼 돌아가");
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...vNode.map(createElement));
+    return fragment;
+  }
+
+  const el = document.createElement(vNode.type);
+  vNode.children.map(createElement).forEach((child) => el.appendChild(child));
+
+  updateAttributes(el, vNode.props);
+  return el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props || {}).map(([property, value]) => {
+    if (property === "className") {
+      $el.setAttribute("class", value);
+    } else {
+      $el.setAttribute(property, value);
+    }
+  });
+}
