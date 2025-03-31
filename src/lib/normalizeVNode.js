0f359c0fd16baa8483f5925aad5204e094b98832
houndhollis <houndhollis9797@gmail.com>
2025-03-31 20:38:15 +0900

    feat: normalizeVNode 기능 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === undefined || vNode === null || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return `${vNode}`;
+  }
+
+  if (typeof vNode.type === "function") {
+    const { type, props, children } = vNode;
+    return normalizeVNode(type({ ...props, children }));
+  }
+
+  return {
+    ...vNode,
+    children: vNode.children.map(normalizeVNode).filter(Boolean),
+  };
 }
