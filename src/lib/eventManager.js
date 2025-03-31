d8bde18ddd354bc21188b72f2c58b51b29a547ce
houndhollis <houndhollis9797@gmail.com>
2025-04-01 01:20:41 +0900

    feat: eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,33 @@
-export function setupEventListeners(root) {}
+const eventHandlers = {};
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  if (!root) return;
 
-export function removeEvent(element, eventType, handler) {}
+  Object.keys(eventHandlers).forEach((eventType) => {
+    root.addEventListener(eventType, handleEvent);
+  });
+}
+
+function handleEvent(event) {
+  const { type, target } = event;
+  if (eventHandlers[type] && eventHandlers[type][target]) {
+    eventHandlers[type][target]();
+  }
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventHandlers[eventType]) {
+    eventHandlers[eventType] = {};
+  }
+  eventHandlers[eventType][element] = handler;
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (!eventHandlers[eventType]) {
+    return;
+  }
+
+  if (eventHandlers[eventType][element] === handler) {
+    delete eventHandlers[eventType][element];
+  }
+}
