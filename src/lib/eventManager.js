66eb13733403ebd94ca46dd99c1d248410554764
houndhollis <houndhollis9797@gmail.com>
2025-04-02 00:14:16 +0900

    chore: 변수명 변경 및 주석제거

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,33 +1,33 @@
-const eventHandlers = {};
+const eventStore = {};
 
 export function setupEventListeners(root) {
   if (!root) return;
 
-  Object.keys(eventHandlers).forEach((eventType) => {
+  Object.keys(eventStore).forEach((eventType) => {
     root.addEventListener(eventType, handleEvent);
   });
 }
 
 function handleEvent(event) {
   const { type, target } = event;
-  if (eventHandlers[type] && eventHandlers[type][target]) {
-    eventHandlers[type][target]();
+  if (eventStore[type] && eventStore[type][target]) {
+    eventStore[type][target]();
   }
 }
 
 export function addEvent(element, eventType, handler) {
-  if (!eventHandlers[eventType]) {
-    eventHandlers[eventType] = {};
+  if (!eventStore[eventType]) {
+    eventStore[eventType] = {};
   }
-  eventHandlers[eventType][element] = handler;
+  eventStore[eventType][element] = handler;
 }
 
 export function removeEvent(element, eventType, handler) {
-  if (!eventHandlers[eventType]) {
+  if (!eventStore[eventType]) {
     return;
   }
 
-  if (eventHandlers[eventType][element] === handler) {
-    delete eventHandlers[eventType][element];
+  if (eventStore[eventType][element] === handler) {
+    delete eventStore[eventType][element];
   }
 }
--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,17 +4,13 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
-
   const normalizedVNode = normalizeVNode(vNode);
 
   if (container.oldNode) {
     updateElement(container, normalizedVNode, container.oldNode);
   } else {
-    const el = createElement(normalizedVNode);
-    container.replaceChildren(el);
+    const element = createElement(normalizedVNode);
+    container.replaceChildren(element);
   }
   container.oldNode = normalizedVNode;
 
