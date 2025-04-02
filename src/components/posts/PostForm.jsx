4193585108fdd411831dd8f3804ff46f83e4c6b4
houndhollis <houndhollis9797@gmail.com>
2025-04-03 00:27:46 +0900

    feat: 좋아요 기능 추가

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,28 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
 export const PostForm = () => {
+  const { currentUser, posts } = globalStore.getState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const content = document.getElementById("post-content").value;
+
+    globalStore.setState({
+      posts: [
+        ...posts,
+        {
+          id: posts.length + 1,
+          author: currentUser?.username,
+          time: new Date(),
+          content: content,
+          likeUsers: [],
+        },
+      ],
+    });
+  };
+
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -10,6 +31,8 @@ export const PostForm = () => {
         className="w-full p-2 border rounded"
       />
       <button
+        role="submit"
+        onclick={onSubmit}
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
       >
